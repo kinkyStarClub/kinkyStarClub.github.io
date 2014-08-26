312d67fb67cd0842fe5e6865817a2af943856c66
davidDuymelinck <david@strass.be>
2014-08-26 17:20:52 +0200

    changed smartphone layout JG podcasts

--- assets/js/jongeduld.js
+++ assets/js/jongeduld.js
@@ -1,6 +1,6 @@
 $(function(){
 	var displayShow = function(i, show){
-			var html = ['<div class="small-3 columns">',
+			var html = ['<div class="small-6 medium-3 columns">',
 						'<a href="'+show.url+'">',
 						'<img src="'+show.pictures.medium+'"><br>',
 						show.name+'</a>',
